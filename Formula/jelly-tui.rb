7d9f8c2c40958267dbff8e08615e7e0d49b0dc24
Luqman Shibly <106410166+Luqman2003@users.noreply.github.com>
2026-08-16 23:52:35 +0000

    Update formula to v1.1.1

--- Formula/jelly-tui.rb
+++ Formula/jelly-tui.rb
@@ -1,17 +1,17 @@
 class JellyTui < Formula
   desc "Terminal Jellyfin client"
   homepage "https://github.com/Luqman2003/jelly-tui"
-  url "https://github.com/Luqman2003/jelly-tui/releases/download/v1.1.0/jelly-tui-1.1.0.jar"
-  sha256 "c4e64c5a50dbfabbaee4fe1a802a78f05657b83301066725b7b4434911fdac03"
+  url "https://github.com/Luqman2003/jelly-tui/releases/download/v1.1.1/jelly-tui-1.1.1.jar"
+  sha256 "46941d53523cb1e91f700bfa55281f0e7d3ba3fd8ecf80651be96bf246ff55af"
 
   depends_on "openjdk@21"
   depends_on "mpv"
 
   def install
-    libexec.install "jelly-tui-1.1.0.jar" => "jelly-tui.jar"
+    libexec.install "jelly-tui-1.1.1.jar" => "jelly-tui.jar"
     (bin/"jelly-tui").write <<~EOS
       #!/bin/bash
-      exec "#{Formula["openjdk@21"].opt_bin}/java" -jar "#{libexec}/jelly-tui.jar" "$@"
+      exec "#{Formula["openjdk@21"].opt_bin}/java" -jar "#{libexec}/jelly-tui.jar" ""
     EOS
   end
 end
