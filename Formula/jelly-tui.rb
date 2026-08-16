ee25dab6eb6d2d8b8860081c40af8d39de5147f4
Luqman Shibly <lshibly@Luqmans-MacBook-Pro.local>
2026-08-16 16:31:08 -0700

    add new version

--- Formula/jelly-tui.rb
+++ Formula/jelly-tui.rb
@@ -2,7 +2,7 @@ class JellyTui < Formula
   desc "Terminal Jellyfin client"
   homepage "https://github.com/Luqman2003/jelly-tui"
   url "https://github.com/Luqman2003/jelly-tui/releases/download/v1.0.0/jelly-tui-1.0.0.jar"
-  sha256 "f5c84927b82e01603bda612025df114f28f84d84d8d20e59fde7fe37afb5066d"
+  sha256 "c4e64c5a50dbfabbaee4fe1a802a78f05657b83301066725b7b4434911fdac03"
 
   depends_on "openjdk@21"
   depends_on "mpv"
